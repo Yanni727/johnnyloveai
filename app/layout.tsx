41565f8195eaca67df0d717481b25a7d39f91216
Yanni727 <johnnyloveai@gmail.com>
2026-03-02 09:18:59 -0500

    Install Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL("https://www.johnnyloveai.com"),
@@ -35,10 +36,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen antialiased">
-        {children}
-      </body>
-    </html>
-  );
-}
+  <html lang="en">
+    <body className="min-h-screen antialiased">
+
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1119244633637458');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1119244633637458&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+
+      {children}
+
+    </body>
+  </html>
+);
